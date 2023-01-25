fbd1a65d01e6ffb79d27fe88df892910437b4eaf
GonzaloGon <a25278@svalero.com>
2023-01-25 21:07:36 +0100

    Algo

--- src/index.js
+++ src/index.js
@@ -1,20 +1,80 @@
 window.onload = () => {
     console.log('loaded')
 
+    let gameOver = false;
+
+    game();
+    /*
     // let cells = <GET ALL cell ELEMENTS>
     let cell = document.getElementsByClassName('cell-x')[0]
     // for (const cell of cells) {
         cell.onclick = (event) => {
             const [, x, y] = event.target.id.split('-')
             console.log(`click on ${x}:${y}`)
-            if (cell.className === 'cell')
+            if(cell.className === 'cell'){
                 cell.className = 'cell-o'
-            else {
-                if (cell.className === 'cell-o')
+            }else{
+                if (cell.className === 'cell-o'){
                     cell.className = 'cell-x'
-                else
+                }else{
                     cell.className = 'cell-o'
+                }  
             }
         }
-    // }
+    // }*/
+    function play(turno) {
+        let cell = document.getElementsByClassName(turno)[0]
+        cell.onclick = (event) => {
+            const [, x, y] = event.target.id.split('-')
+            console.log(`click on ${x}:${y}`)
+            if(cell.className == 'cell'){
+                cell.className = turno;
+            }else{
+                if (cell.className === 'cell-o'){
+                    cell.className = 'cell-x'
+                }else{
+                    cell.className = 'cell-o'
+                }  
+            }
+        }
+    }
+
+    function game() {
+        let i = 0;
+        
+        while (gameOver == false && i<=8) {
+            if (i%2 == 0) {
+                //Turno de O
+                document.getElementsById('turn').className="turn-cell-o"
+                play('cell-o');
+
+            }else{
+                //Turno de X
+                document.getElementsById('turn').className="turn-cell-x"
+                play('cell-x');
+            }
+            i++;
+        }
+        if(i>8){
+            //Emapte
+        }
+        //function acabar juego
+    }
+
+    function end(){
+
+    }
+
+    function win(player) {
+        
+    }
 }
+
+
+/* TO DO:
+-ALTERNAR CIRCULO Y CRUZ
+-CLICK SOBRE CASILLA
+-CONDICIÓN DE VICTORIA
+-FINAL GANAR
+-FINAL EMPATAR
+ */
